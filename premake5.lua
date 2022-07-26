5cd7657b0ea91f1305a510f1887597b97f016ff2
Rafis Rakhmankulov <rakhmankulov.r@gmail.com>
2022-07-26 21:23:52 +0300

    Add premake build configuration.

---
+++ premake5.lua
@@ -0,0 +1,109 @@
+project "GLFW"
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
+
+	targetdir (TargetDir)
+	objdir (ObjDir)
+
+	files{
+		"include/GLFW/glfw3.h",
+		"include/GLFW/glfw3native.h",
+		"src/glfw_config.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+
+        "src/egl_context.c",
+        "src/osmesa_context.c",
+
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+
+		"src/platform.c",
+		"src/vulkan.c",
+		"src/window.c",
+	}
+
+	-- filter "platforms:Linux"
+	-- 	pic "On"
+
+	-- 	systemversion "latest"
+		
+	-- 	files {
+	-- 		"src/x11_init.c",
+	-- 		"src/x11_monitor.c",
+	-- 		"src/x11_window.c",
+	-- 		"src/xkb_unicode.c",
+	-- 		"src/posix_time.c",
+	-- 		"src/posix_thread.c",
+	-- 		"src/glx_context.c",
+	-- 		"src/linux_joystick.c"
+	-- 	}
+
+	-- 	defines {
+	-- 		"_GLFW_X11"
+	-- 	}
+
+    -- filter "platforms:Windows"
+	-- 	systemversion "latest"
+
+	-- 	files {
+	-- 		"src/win32_init.c",
+	-- 		"src/win32_joystick.c",
+	-- 		"src/win32_module.c",
+	-- 		"src/win32_monitor.c",
+	-- 		"src/win32_time.c",
+	-- 		"src/win32_thread.c",
+	-- 		"src/win32_window.c",
+	-- 		"src/wgl_context.c",
+	-- 	}
+
+	-- 	defines { 
+	-- 		"_GLFW_WIN32",
+	-- 		"_CRT_SECURE_NO_WARNINGS"
+	-- 	}
+
+	-- 	links {
+	-- 		"Dwmapi.lib"
+	-- 	}
+
+    -- For some reason filters for files doesn't work on macos
+    filter "platforms:MacOS"
+        files {
+            "src/cocoa_init.m",
+            "src/cocoa_joystick.m",
+            "src/cocoa_monitor.m",
+            "src/cocoa_window.m",
+            "src/cocoa_time.c",
+            "src/posix_module.c",
+            "src/posix_thread.c",
+            "src/nsgl_context.m"
+        }
+
+        defines { 
+            "_GLFW_COCOA"
+        }
+
+		links {
+			"CoreFoundation.framework",
+			"IOKit.framework",
+			"Cocoa.framework"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Development"
+		runtime "Debug"
+		optimize "on"
+		symbols "on"
+
+	filter "configurations:Shipping"
+		runtime "Release"
+		optimize "Full"
+        symbols "off"
