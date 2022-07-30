effe6c19695810dc4c1d2795c2b59f14a00788b1
Rafis Rakhmankulov <rakhmankulov.r@gmail.com>
2022-07-30 04:39:22 +0300

    Fix linking issue for Windows.

--- premake5.lua
+++ premake5.lua
@@ -3,28 +3,31 @@ project "GLFW"
 	language "C"
 	staticruntime "off"
 
-	targetdir (TargetDir)
-	objdir (ObjDir)
+	targetdir (LibTargetDir)
+	objdir (LibObjDir)
 
 	files{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
+		"src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
-
-		"src/egl_context.c",
-		"src/osmesa_context.c",
-
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-
 		"src/platform.c",
 		"src/vulkan.c",
 		"src/window.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c",
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
+	
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c",
 	}
 
 	-- filter "platforms:Windows"
@@ -33,13 +36,15 @@ project "GLFW"
 
 		files {
 			"src/win32_init.c",
+			"src/win32_module.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
+			"src/win32_time.h",
 			"src/win32_time.c",
+			"src/win32_thread.h",
 			"src/win32_thread.c",
 			"src/win32_window.c",
 			"src/wgl_context.c",
-			"src/egl_context.c",
 		}
 
 		defines { 
@@ -82,7 +87,7 @@ project "GLFW"
 			"src/cocoa_time.c",
 			"src/posix_module.c",
 			"src/posix_thread.c",
-			"src/nsgl_context.m"
+			"src/nsgl_context.m",
 		}
 
 		defines { 
